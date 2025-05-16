1682d11aa050d1adf7c3bf57e8d8777b5c40438f
Gijs0 <Gijsvanbelkom08@gmail.com>
2025-05-16 09:59:34 +0200

    New Good

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your Name</title>
+    <!-- Styles -->
+    <style>
+        /* Your CSS here */
+        body {
+            font-family: 'Nunito', sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f3f4f6;
+        }
+        .content {
+            text-align: center;
+        }
+        h1 {
+            font-size: 3rem;
+            color: #2d3748;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <h1>Gijs van Belkom</h1>
+        <p>Welcome to my Laravel application!</p>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\HomeController;
+
+Route::get('/home', function () {
+    return view('home');
 });
